c2ce1ab0dd0def8d4e2f361cf742cd87b1f121c8
unknown <rtakanami0706@gmail.com>
2020-08-04 12:46:27 +0900

    独自ユーザークラスの作成

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -5,10 +5,11 @@ import java.security.Principal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.domain.model.AppUserDetails;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HomeController {
 	@GetMapping("/home")
-	public String getHome(Model model, @AuthenticationPrincipal User user) {
+	public String getHome(Model model, @AuthenticationPrincipal AppUserDetails user) {
 		log.info("HomeController Start");
 
 		// ログインユーザー情報の表示
@@ -34,11 +35,11 @@ public class HomeController {
 	public String getHome2(Model model, Principal principal) {
 		// ログインユーザー情報の取得(その1)
 		Authentication authentication = (Authentication) principal;
-		User user1 = (User) authentication.getPrincipal();
+		AppUserDetails user1 = (AppUserDetails) authentication.getPrincipal();
 		log.info("user1: " + user1.toString());
 
 		// ログインユーザー情報の取得(その2)
-		User user2 = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		AppUserDetails user2 = (AppUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
 		log.info("user2: " + user2.toString());
 
